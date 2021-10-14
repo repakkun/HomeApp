b9d2eb6a6efac990b47f9524bd52580e6c8f913d
Nastya <Nastya@LAPTOP-7C1P7I5Q>
2021-10-14 22:25:34 +0300

    Добавьте файлы проекта.

---
+++ HomeApp/HomeApp/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace HomeApp.Pages
+{
+    public partial class RegisterPage : ContentPage
+    {
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
